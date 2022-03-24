942284959346d9c7cf2d340afdc66318b7fd0e87
MelRobles <testMelEmail@test.com>
2022-03-23 22:02:39 -0600

    Part 2 code without updated database

--- TempleTourGenius/Controllers/HomeController.cs
+++ TempleTourGenius/Controllers/HomeController.cs
@@ -38,8 +38,8 @@ namespace TempleTourGenius.Controllers
         [HttpGet]
         public IActionResult Form(int timeid)
         {
-            ViewBag.TimeSlots = _slots.Timeslots.ToList();
-            var appointment = _slots.Timeslots.Single(x => x.TimeId == timeid);
+            //ViewBag.TimeSlots = _slots.Timeslots.ToList();
+            //var appointment = _slots.Timeslots.Single(x => x.TimeId == timeid);
             //do i need to have "Form", appointment if it's in the same view?
             return View(appointment);
         }
